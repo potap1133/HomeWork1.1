50edc94e2ce8b40f3ec65e91462ca31e515a4dbe
potap1133 <potap1133@gmail.com>
2019-05-21 12:52:32 +0300

    new

--- 06/HomeWork6/HomeWork_6/Program.cs
+++ 06/HomeWork6/HomeWork_6/Program.cs
@@ -6,29 +6,37 @@ namespace HomeWork_6
     {
         static void Main(string[] args)
         {
+            double num;
+            double num1 = 0;
+            short num2;
 
-            
             Console.WriteLine(" Enter a positive positive natural number not exceeding 2 billion: ");
-            var num1 = 0;
+
             do
             {
                 try
                 {
-                    var num = int.Parse(Console.ReadLine());
-                    
-                    num1 = num % 10;
-                                        
+                    num = double.Parse(Console.ReadLine());
+
+                    num1 = num % 10;                    
+                    num2 =(short)(num1 % 2);
+                    num1 = (int)(num1 / 10);
+
+
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine(num1);
+                    }
+
 
                 }
-                catch
+                catch (FormatException)
                 {
+                    Console.WriteLine();
                 }
 
             } while (true);
-            if (num1 / 2 == 0)
-            {
-                Console.WriteLine(num1);
-            }
+            
 
 
 
