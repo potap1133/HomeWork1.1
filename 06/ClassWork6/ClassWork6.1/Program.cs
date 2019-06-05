9b144ea32804c488ac3fd589b1ad0e261fb0d499
potap1133 <potap1133@gmail.com>
2019-06-05 10:26:55 +0300

    new

--- 06/ClassWork6/ClassWork6.1/Program.cs
+++ 06/ClassWork6/ClassWork6.1/Program.cs
@@ -10,9 +10,7 @@ namespace ClassWork7._1
     {
         static void Main(string[] args)
         {
-
-
-
+           
             var num = new[]
             {
             new[] { 5, 6, 2, 3, 6 },
@@ -22,18 +20,36 @@ namespace ClassWork7._1
             new[] { 6, 7, 9 },
             new[] { 7, 9 },
             };
-            /*if (num != null)*/
 
-                for (int i = 0; i < 6; i++)
+            //var num2 = 0;
+            //var num1 = 0;
+
+            double num5 = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                double num2 = 0;
+                //var num1 = 0;
+                if (num[i] == null)
                 {
-                  var num1 = num[i];
-                  for (int j = 0; j < num1.Length; j++)
-
-                    num1[j] += num1[j];
- 
-                        Console.WriteLine($"Days {num[i]}, average mark {num1} ");
-
+                    Console.WriteLine("The average mark for day: N/A");
+                    continue;
                 }
+
+                var num3 = i;
+                var num1 = num[i];
+                for (int j = 0; j < num1.Length; j++)
+                {
+
+                     num2 += (double) (num1[j] * 1.0 / num1.Length);
+                     
+                }
+                num5 = (num2 + num2) / num.Length;
+                Console.WriteLine("The average mark for day: {0} is {1:f}", num3, num2);
+                
+                continue;                
+            }
+            Console.WriteLine("The average mark for week: {0:f} ", num5 );
+
             Console.ReadKey();
         }
     }
