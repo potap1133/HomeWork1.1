f4fe36b67664d3dc37cb0fae46ee7ac420948d29
potap1133 <potap1133@gmail.com>
2019-06-06 09:07:06 +0300

    new

---
+++ 10/HomeWork10/HomeWork10/Man.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeWork10
+{
+    public class Man
+    {
+        public string Name { get; set; }
+
+        int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age +4;    
+            }
+
+            set
+            {
+               
+                _age = value;
+               
+            }
+
+        }
+
+    }
+}
---
+++ 10/HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name : ");
+            var nameFist = Console.ReadLine();
+            Console.WriteLine("Enter your age :");
+            int yearsFist = int.Parse(Console.ReadLine());
+
+            Man num = new Man() { Name = nameFist, Age = yearsFist };
+           
+                       
+            Console.WriteLine($"{num.Name}, {num.Age} ");
+            Console.ReadKey();
+        }
+    }
+}
